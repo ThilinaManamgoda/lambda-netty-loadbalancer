c09663bcab353741a274293bcf4b620dabecc871
maanadev <maanafunedu@gmail.com>
2017-05-20 09:59:24 +0530

    FullHttpRequest support is adde

--- src/main/java/lambda/netty/loadbalancer/core/SysService/SysServiceResponseHandler.java
+++ src/main/java/lambda/netty/loadbalancer/core/SysService/SysServiceResponseHandler.java
@@ -2,16 +2,14 @@ package lambda.netty.loadbalancer.core.SysService;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.LastHttpContent;
 import io.netty.util.CharsetUtil;
 
-/**
- * Created by maanadev on 5/19/17.
- */
 public class SysServiceResponseHandler extends SimpleChannelInboundHandler<HttpObject> {
-    ;
+
 
     public SysServiceResponseHandler() {
 
@@ -20,16 +18,11 @@ public class SysServiceResponseHandler extends SimpleChannelInboundHandler<HttpO
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
 
-        if (msg instanceof HttpContent) {
-            HttpContent content = (HttpContent) msg;
+        if (msg instanceof FullHttpRequest) {
+            FullHttpRequest fullHttpRequest = (FullHttpRequest) msg;
 
-            System.err.print(content.content().toString(CharsetUtil.UTF_8));
+            System.out.println(fullHttpRequest.headers().get("Host"));
 
-            System.err.flush();
-
-            if (content instanceof LastHttpContent) {
-                ctx.close();
-            }
         }
     }
 }
