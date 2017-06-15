7014bd7b6a18d85ec461dae4a55d41df080be11f
maanadev <maanafunedu@gmail.com>
2017-06-16 00:08:40 +0530

    script is removed

--- install-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://github.com/ThilinaManamgoda/jetcd/archive/master.zip
-unzip master.zip -d .
-cd jetcd-master
-mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
