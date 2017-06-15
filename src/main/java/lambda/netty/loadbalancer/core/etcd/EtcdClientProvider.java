4cd599cf89ac5a012529655d7a8c91937ba48aad
maanadev <maanafunedu@gmail.com>
2017-06-16 03:15:59 +0530

    code formatting

--- src/main/java/lambda/netty/loadbalancer/core/etcd/EtcdClientProvider.java
+++ src/main/java/lambda/netty/loadbalancer/core/etcd/EtcdClientProvider.java
@@ -9,6 +9,7 @@ import com.coreos.jetcd.api.RangeResponse;
 import com.coreos.jetcd.data.ByteSequence;
 import com.coreos.jetcd.exception.AuthFailedException;
 import com.coreos.jetcd.exception.ConnectException;
+import org.apache.log4j.Logger;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
@@ -18,11 +19,26 @@ import java.util.concurrent.ExecutionException;
  * Created by maanadev on 6/15/17.
  */
 public class EtcdClientProvider {
+    final static Logger logger = Logger.getLogger(EtcdClientProvider.class);
+
+   private static Client client = null;
+
+    public KV newEtcdKVClient(){
+        if(client==null){
+            try {
+                client = ClientBuilder.newBuilder().endpoints("http://localhost:2379").build();
+            } catch (ConnectException e) {
+                e.printStackTrace();
+            } catch (AuthFailedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) throws AuthFailedException, ConnectException, ExecutionException, InterruptedException {
-           Client client = null;
 
-            client = ClientBuilder.newBuilder().endpoints("http://localhost:2379").build();
+
+
 
         KV kvClient = client.getKVClient();
         ByteSequence key = ByteSequence.fromString("test_key");
